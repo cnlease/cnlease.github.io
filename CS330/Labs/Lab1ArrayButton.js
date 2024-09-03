2290d1d4375229dec7daf96e79c5e5085795a487
cnlease <cnlease@gmail.com>
2024-09-03 09:28:31 -0400

    Update Lab1ArrayButton.js

--- CS330/Labs/Lab1ArrayButton.js
+++ CS330/Labs/Lab1ArrayButton.js
@@ -3,5 +3,5 @@ function ArrayWork(){
     var array =[];
     array.push(Math.floor(Math.random() * 100));
 
-    document.getElementById("ArrayOut").innerHTML(array[0]);
+    document.getElementById("ArrayOut").innerHTML("Hello");
 }
