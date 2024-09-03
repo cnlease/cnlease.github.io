346873b22029aa530c777f016eb8e25cd8da6ded
cnlease <cnlease@gmail.com>
2024-09-03 09:52:11 -0400

    Update Lab1ArrayButton.js

--- CS330/Labs/Lab1ArrayButton.js
+++ CS330/Labs/Lab1ArrayButton.js
@@ -1,21 +1,25 @@
 //Function to create an array with random numbers and do math
 //Need to calculate the mean of the array, and which elements are larger than the mean.
 function ArrayWork(){
-    var array =[];
-    var mean = 0;
-    for(var i =0; i<5; i++){
+    var array =[]; //Array to hold the 5 random numbers
+    var mean = 0; //variable to hold the mean
+    for(var i =0; i<5; i++){ //fills the array with random & starts to calculate the mean
         array.push(Math.floor(Math.random() * 100));
         mean += array[i];
     }
-    document.getElementById("ArrayOut").innerHTML = "The random array: " + array;
-    mean /= array.length;
-    document.getElementById("ArrayOut").innerHTML = "The Mean: " + mean;
-    var biggerthan =[];
-    for(var i =0; i<5; i++){
+    var output = "The random array: " + array; //variable to output a string
+    
+    mean /= array.length; //divides by length
+
+    output += "\nThe mean: " + mean + "\nThe larger numbers: "; //adds mean to the output
+    
+    
+    for(var i =0; i<5; i++){ //adds the larger than the mean numbers
         if(array[i] > mean){
-            biggerthan.push(array[i]);
+            output += array[i] + " ";
         }
     }
-    document.getElementById("ArrayOut").innerHTML = "The elements that are larger: " + biggerthan;
+    document.getElementById("ArrayOut").innerHTML = output; //changes the website to output
+    
 
 }
