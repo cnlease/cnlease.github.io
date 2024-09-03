0c0b673efebb3ae6757521a39b2ace855fd2205c
cnlease <cnlease@gmail.com>
2024-09-03 09:40:30 -0400

    Update Lab1ArrayButton.js

--- CS330/Labs/Lab1ArrayButton.js
+++ CS330/Labs/Lab1ArrayButton.js
@@ -1,7 +1,8 @@
 //Function to create an array with random numbers and do math
 function ArrayWork(){
     var array =[];
-    array.push(Math.floor(Math.random() * 100));
-
-    document.getElementById("ArrayOut").innerHTML = array[0];
+    for(var i =0; i<5; i++){
+        array.push(Math.floor(Math.random() * 100));
+    }
+    document.getElementById("ArrayOut").innerHTML = array;
 }
