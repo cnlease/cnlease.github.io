c1c8f9ce2e7c05576ff00e4b4c14efce003f127a
cnlease <cnlease@gmail.com>
2024-09-09 22:20:09 -0400

    making lab 2

--- CS330/Labs/Lab2Date.js
+++ CS330/Labs/Lab2Date.js
@@ -1,1 +1,28 @@
 //lab 2 javascript file
+function DatePage(){
+    var month = getMonth();
+    var date = getDate();
+    var hours = getHours();
+    var minutes = getMinutes();
+    var year = getYear();
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    var print = "The date is: " + months[month] + " " + date + ", " + year;
+    
+    var e = document.getElementById("timeformat");
+    var value = e.value;
+
+    if(value == "24"){
+        print += "<br /> Time: " + hours + ":" + minutes;
+    }else{
+        print += "<br /> Ha failed";
+    }
+
+    document.getElementById("TimeOutput").innerHTML = print;
+
+}
+
+function DateAlert(){
+
+}
