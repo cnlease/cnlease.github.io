b5e514021052bd5b8051a2faecaaa1c32c029263
cnlease <cnlease@gmail.com>
2024-09-09 22:41:59 -0400

    Update Lab2Date.js

--- CS330/Labs/Lab2Date.js
+++ CS330/Labs/Lab2Date.js
@@ -1,33 +1,32 @@
 //lab 2 javascript file
-function DatePage(){
-    const date = new Date();
-    var month = date.getMonth();
-    var day = date.getDate();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var year = date.getYear();
+const date = new Date();
+var month = date.getMonth();
+var day = date.getDate();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var year = date.getFullYear();
 
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    var print = "The date is: " + months[month] + " " + day + ", " + year;
+var print = "The date is: " + months[month] + " " + day + ", " + year;
     
-    var e = document.getElementById("timeformat");
-    var value = e.value;
-    console.log(value);
+var e = document.getElementById("timeformat");
+var value = e.value;
+console.log(value);
 
-    if(value == 24){
-        print += "<br /> Time: " + hours + ":" + minutes;
-    }else{
-        if(hours > 12){
-            hours -= 12;
-        }
-        print += "<br /> Time: " + hours +":" + minutes;
+if(value == 24){
+    print += "<br /> Time: " + hours + ":" + minutes;
+}else{
+    if(hours > 12){
+        hours -= 12;
     }
-
+    print += "<br /> Time: " + hours +":" + minutes;
+}
+function DatePage(){
     document.getElementById("TimeOutput").innerHTML = print;
 
 }
 
 function DateAlert(){
-
+    window.alert(print);
 }
