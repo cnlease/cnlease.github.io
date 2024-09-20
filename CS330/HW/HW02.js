164417a9c43516db34d65a3762728f1c79101ab3
cnlease <cnlease@gmail.com>
2024-09-20 00:27:07 -0400

    Update HW02.js

--- CS330/HW/HW02.js
+++ CS330/HW/HW02.js
@@ -17,31 +17,26 @@ function init()
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
 
-    //
-    //  Initialize our data for the Sierpinski Gasket
-    //
 
-    // First, initialize the vertices of our 3D gasket
-    // Four vertices on unit circle
-    // Intial tetrahedron with equal length sides
+    // First, initialize the vertices
 
     var vertices = [
-        vec3(0.0000,  0.0000, -1.0000),
-        vec3(0.0000,  0.9428,  0.3333)
+        vec2(0, 0.5),
+        vec2(1.0,  0.5)
     ];
 
-    divideTetra(vertices[0], vertices[1], vertices[2], vertices[3],
-                 numTimesToSubdivide);
 
     //
     //  Configure WebGL
     //
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
-    // enable hidden-surface removal
+    //  Load shaders and initialize attribute buffers
 
-    gl.enable(gl.DEPTH_TEST);
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
 
     //  Load shaders and initialize attribute buffers
 
@@ -70,63 +65,10 @@ function init()
     render();
 };
 
-function triangle( a, b, c, color )
+function divideLine(a, b, count)
 {
-
-    // add colors and vertices for one triangle
-
-    var baseColors = [
-        vec3(1.0, 0.0, 0.0),
-        vec3(0.0, 1.0, 0.0),
-        vec3(0.0, 0.0, 1.0),
-        vec3(0.0, 0.0, 0.0)
-    ];
-
-    colors.push(baseColors[color]);
-    positions.push(a);
-    colors.push(baseColors[color]);
-    positions.push(b);
-    colors.push(baseColors[color]);
-    positions.push(c);
-}
-
-function tetra( a, b, c, d )
-{
-    // tetrahedron with each side using
-    // a different color
-
-    triangle(a, c, b, 0);
-    triangle(a, c, d, 1);
-    triangle(a, b, d, 2);
-    triangle(b, c, d, 3);
-}
-
-function divideTetra(a, b, c, d, count)
-{
-    // check for end of recursion
-
-    if (count === 0) {
-        tetra(a, b, c, d);
-    }
-
-    // find midpoints of sides
-    // divide four smaller tetrahedra
-
-    else {
-        var ab = mix(a, b, 0.5);
-        var ac = mix(a, c, 0.5);
-        var ad = mix(a, d, 0.5);
-        var bc = mix(b, c, 0.5);
-        var bd = mix(b, d, 0.5);
-        var cd = mix(c, d, 0.5);
-
-        --count;
-
-        divideTetra(a, ab, ac, ad, count);
-        divideTetra(ab,  b, bc, bd, count);
-        divideTetra(ac, bc,  c, cd, count);
-        divideTetra(ad, bd, cd,  d, count);
-    }
+    
+    
 }
 
 
