20f9c22a392f638673e388bfde71e4127792b17f
cnlease <cnlease@gmail.com>
2024-09-20 00:49:23 -0400

    Update HW02.js

--- CS330/HW/HW02.js
+++ CS330/HW/HW02.js
@@ -1,31 +1,20 @@
 "use strict";
-
-var canvas;
 var gl;
-
-var positions = [];
-var colors = [];
-
-var numTimesToSubdivide = 3;
-
+var points;
 init();
 
 function init()
 {
-    canvas = document.getElementById("gl-canvas");
+    var canvas = document.getElementById( "gl-canvas" );
 
     gl = canvas.getContext('webgl2');
-    if (!gl) alert("WebGL 2.0 isn't available");
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-
-    // First, initialize the vertices
-
-    points = [
-        vec2(0, 0.5),
-        vec2(1.0,  0.5)
+    points=[
+    vec2( -0.95 , -0.95 ),
+    vec2(  0.00 , -0.55 ),
     ];
-
-
+    
     //
     //  Configure WebGL
     //
@@ -37,48 +26,23 @@ function init()
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    // Load the data into the GPU
 
-    //  Load shaders and initialize attribute buffers
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    // Associate out shader variables with our data buffer
 
-    // Create a buffer object, initialize it, and associate it with the
-    //  associated attribute variable in our vertex shader
-
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-    var colorLoc = gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(colorLoc);
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
-
-    var positionLoc = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
-
-    document.getElementById("slider").onchange = function(event) {
-        sliderVal = parseInt(event.target.value);
-        render();
-    };
+    var positionLoc = gl.getAttribLocation( program, "aPosition" );
+    gl.vertexAttribPointer( positionLoc , 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( positionLoc );
 
     render();
 };
 
-function divideLine(a, b, count)
-{
-    
-    
-}
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.LINES, 0, points.length );
 
-
-function render()
-{
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);    
-    gl.drawArrays(gl.LINES, 1, points.length);
 }
