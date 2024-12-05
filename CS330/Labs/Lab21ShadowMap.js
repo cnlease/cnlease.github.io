021c768eddcece6067450d42d34e6f815ddfb715
cnlease <cnlease@gmail.com>
2024-12-04 22:17:22 -0500

    Update Lab21ShadowMap.js

--- CS330/Labs/Lab21ShadowMap.js
+++ CS330/Labs/Lab21ShadowMap.js
@@ -10,7 +10,7 @@ var gl;
 
 window.onload = init;
 
-var numCubeVertices  = 36;
+var numCubeVertices  = 6;
 var numTriangleVertices = 3;
 
 var triangleInstanceMatrix, cubeInstanceMatrix;
@@ -92,14 +92,9 @@ function quad(a, b, c, d) {
      colorsArray.push(cubeColors[a]);
 }
 
-function colorCube()
+function colorPlane()
 {
-    quad(0, 1, 5, 4);
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
     quad(4, 5, 6, 7);
-    quad(5, 1, 2, 6);
 }
 
 function triangle(a, b, c) {
@@ -145,7 +140,7 @@ function init() {
 
 // generate cube and triangle data
 
-    colorCube();
+    colorPlane();
     triangle(0, 1, 2);
 
 //  Load shaders and initialize attribute buffers
@@ -309,6 +304,6 @@ function render() {
             "uInstanceMatrix"), false, flatten(triangleInstanceMatrix));
     gl.drawArrays(gl.TRIANGLES, numCubeVertices, numTriangleVertices);
 
-    requestAnimationFrame(render)
+    setInterval( requestAnimationFrame(render) , 500) ;
 
 }
